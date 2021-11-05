5a83ba0f75db9db90c6d80f3e0cf43d15f1100b4
dimitrc <dimitrc@stud.ntnu.no>
2021-11-05 13:50:56 +0100

    TopN untested

---
+++ Backend/CSAMS/APIModels/TopModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSAMS.APIModels
+{
+    public class TopModel : IAPIModel
+    {
+        public float Grade { get; set; }
+        public string AssingmentName { get; set; }
+        public int AssingmentID { get; set; }
+
+       
+
+        public string type { get; set; }
+        public bool AssertEqual(IAPIModel other)
+        {
+            var otherProject = other as TopModel;
+            return (Grade == otherProject.Grade &&
+                AssingmentName == otherProject.AssingmentName &&
+                type == otherProject.type &&
+               
+                AssingmentID == otherProject.AssingmentID);
+        }
+    }
+}
---
+++ Backend/Test-Framework/GlobalStatistics/TopTests.cs
@@ -0,0 +1,59 @@
+﻿using CSAMS.APIModels;
+using CSAMS.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Test_Framework.Methods;
+
+
+
+namespace Test_Framework.GlobalStatistics
+{
+    [TestClass]
+    public class TopTests
+    {
+        public readonly TopModel[] ExpectedProjectResultOne =
+            {
+                new TopModel{Grade= 40,AssingmentName= "Cloud 2021 - Assignment 2",AssingmentID= 6,type= "Top"},
+               new TopModel{Grade= 5,AssingmentName= "PROG2006 Assignment 1 tic-tac-roll",AssingmentID= 5,type= "Top"},
+             new TopModel{Grade= 40,AssingmentName= "Cloud 2021 - Assignment 1",AssingmentID= 1,type= "Top"}
+            };
+
+        public readonly TopModel[] ExpectedProjectResultTwo = 
+        {
+                    new TopModel{Grade= 1,AssingmentName= "Cloud 2021 - Assignment 1",AssingmentID= 1,type= "Bottom"},
+               new TopModel{Grade= 1,AssingmentName= "PROG2006 Assignment 1 tic-tac-roll",AssingmentID= 5,type= "Bottom"},
+             new TopModel{Grade= 1,AssingmentName= "Cloud 2021 - Assignment 2",AssingmentID= 6,type= "Bottom"}
+            
+        
+        };
+
+
+
+        
+        [TestMethod]
+        public void TestProjectTopsOne()
+        {
+            var t = TestData.TestData.TopTests.GroupBy(r => r.AssignmentID).Select(r => TopController.TopProjects(r.ToArray(), TestData.TestData.TopTestsFields)).ToArray();
+
+            // var result = TopController.TopProjects(t[i], TestData.TestData.TopTestsFields);
+
+            
+               
+           // for(int i=0;i<t.Length;i++)
+                Assert.IsTrue(EqualClassChecker.APIModelEqual(ExpectedProjectResultOne, t));
+            
+        }
+
+        [TestMethod]
+        public void TestProjectTopsTwo()
+        {
+            var result = TopController.BottomProjects(TestData.TestData.TopTests, TestData.TestData.TopTestsFields);
+            Assert.IsTrue(EqualClassChecker.APIModelEqual(ExpectedProjectResultTwo, result));
+        }
+
+            
+    }
+}
+    

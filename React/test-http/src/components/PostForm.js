8c2cc9cbd1237a69cf83fa1a18c920ed3efc6ee5
Jonas Hægdahl <jonas.haegd@gmail.com>
2021-11-12 19:43:39 +0100

    Ready for backend implementation

--- React/test-http/src/components/PostForm.js
+++ React/test-http/src/components/PostForm.js
@@ -1,51 +1,51 @@
 import React, { Component } from 'react'
 import axios from 'axios'
- class PostForm extends Component {
+class PostForm extends Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             id:'',
-             role:''
+            id: '',
+            role: ''
         }
     }
-    
-changeHandler=(e)=>{
-    this.setState({[e.target.name]:e.target.value})
-}
 
-submitHandler=(e)=>{
-    e.preventDefault()
-    console.log(this.state)
-   
- 
-    axios.post('https://localhost:44344/api/sample/post', this.state, {
-        headers: {
-            'content-type': 'application/json',
-            "Access-Control-Allow-Origin": "*"
-        }
-    })
-    .then(Response=> {
-        console.log(Response)
-    })
-    .catch(error=>{
-        console.log(error)
-    })
-     }
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    submitHandler = (e) => {
+        e.preventDefault()
+        console.log(this.state)
+
+
+        axios.post('https://localhost:44344/api/sample/post', this.state, {
+            headers: {
+                'content-type': 'application/json',
+                "Access-Control-Allow-Origin": "*"
+            }
+        })
+            .then(Response => {
+                console.log(Response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
 
     render() {
-        const{id,role}=this.state
+        const { id, role } = this.state
         return (
-            <div style={{border: 'outset',textAlign:'center'}}>
+            <div style={{ border: 'outset', textAlign: 'center' }}>
                 <form onSubmit={this.submitHandler}>
                     <div>
-                        <input type='text' name='id' value={id} onChange={this.changeHandler} placeholder='ID'/>
+                        <input type='text' name='id' value={id} onChange={this.changeHandler} placeholder='ID' />
                     </div>
                     <div>
-                        <input type='text' name='role' value={role}  onChange={this.changeHandler} placeholder='RoleID'/>
+                        <input type='text' name='role' value={role} onChange={this.changeHandler} placeholder='RoleID' />
                     </div>
-                   
+
                     <button type='submit'>Submit </button>
                 </form>
             </div>

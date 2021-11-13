d27feb2871a7d8906a9f868fad212deb3b429ac3
dimitrc <dimitrc@stud.ntnu.no>
2021-11-13 16:43:46 +0100

    new shinny (purple) buttons

--- React/test-http/src/components/Top.js
+++ React/test-http/src/components/Top.js
@@ -9,14 +9,14 @@ class Top extends Component {
             posts: [],
             errorMsg: '',
             N:'',
-            Type:'',
-            IsProject:'',
+            Type:'Top',
+            IsProject:false,
             site:'https://localhost:44344/api/Top/top',
             Submited:false,
             TopsClicked:false,
-            BottomsClicked:false,
            ProjectsClicked:false,
-           ReviewersClicked:false
+          
+         
         }
     }
   
@@ -37,7 +37,7 @@ doSomething(){
            
            if(this.state.Submited===true){
             axios
-                .get(this.state.site)
+                .get(this.state.site+'/'+this.state.N+'/'+this.state.Type+'/'+this.state.IsProject)
                 .then(response => {
                     console.log(response)
                     this.setState({ posts: response.data })
@@ -50,36 +50,17 @@ doSomething(){
                // this.setState({Submited:false})
            }
         }
-           dosomethingmore(){
-            this.setState({site:this.state.site+'/'+this.state.N+'/'+this.state.Type+'/'+this.state.IsProject})
-            this.setState({Submited:true})
-
-            }
+           
         submitHandler = (e) => {
             this.setState({[e.target.name]:e.target.value})
             
-
-          //  this.setState({Type:this.Capitalize(this.state.Type)})
-         //   this.setState({IsProject:this.Capitalize(this.state.IsProject)})     
+   
             if(this.state.N<1||this.state.N>300){
                 this.setState({ errorMsg2: ' Error in imput field 1. Provide possitive number (within reason)' })
             }
-            // this.setState({Type:this.Capitalize(this.state.Type)})
-            // if(this.state.Type!=='Top'||this.state.Type!=='Bottom'){
-            //     this.setState({ errorMsg2: ' Error in imput field 2' })
-            // }
-            // this.setState({IsProject:this.Capitalize(this.state.IsProject)})
-        
-            // if(this.state.IsProject==='Projects' || this.Capitalize(this.state.IsProject)==='True'){
-            //     this.setState({IsProject:'true'})
-            // }else if(this.state.IsProject==='Reviewers' || this.Capitalize(this.state.IsProject)==='False'){
-            //     this.setState({IsProject:'false'})
-            // }
-            // else{
-            //     this.setState({ errorMsg2: ' Error in imput field 3' })
-            // }
-            this.setState({Submited:true})
-            this.setState({site:this.state.site+'/'+this.state.N+'/'+this.state.Type+'/'+this.state.IsProject},()=>{ this.doSomething()})
+         
+            this.setState({Submited:true},()=>{ this.doSomething()})
+           // this.setState({site:this.state.site+'/'+this.state.N+'/'+this.state.Type+'/'+this.state.IsProject},()=>{ this.doSomething()})
           
            
             e.preventDefault()
@@ -93,9 +74,20 @@ doSomething(){
             this.setState({Type:EventTarget.value})
             console.log(this.state)
         }
+        changeColorIs(){
+            this.setState({ProjectsClicked: !this.state.ProjectsClicked})
+        }
 
+        changeColorType(){
+            this.setState({TopsClicked: !this.state.TopsClicked})
+            console.log(this.state+"COLOR CHANGE!!")
+        }
 
     render() {
+        let btn_classtype = this.state.TopsClicked ? "blackButton" : "whiteButton";
+        let btn_classIs = this.state.ProjectsClicked ? "blackButton" : "whiteButton";
+        let btn_classIs2 = this.state.ProjectsClicked ? "whiteButton" : "blackButton" ;
+        let btn_classtype2 = this.state.TopsClicked ? "whiteButton" : "blackButton" ;
         const { posts, errorMsg,errorMsg2,N,Type,IsProject } = this.state
         let n = 0
 
@@ -105,22 +97,25 @@ doSomething(){
         } else {
             console.log(this.state)
         message=  <div style={{ textAlign: 'center' }}>
+          
     <tr style={{ display: 'flex', justifyContent: 'center' }}>
       <td style={{ border: "1px solid rgb(0, 0, 0)", width: 300 }}>Grade</td>
      <td style={{ border: "1px solid rgb(0, 0, 0)", width: 300 }}>Assignment name</td>
      <td style={{ border: "1px solid rgb(0, 0, 0)", width: 200 }}>Assignment ID</td>
      <td style={{ border: "1px solid rgb(0, 0, 0)", width: 200 }}>Reviewer ID</td>
 
-</tr>
+        </tr>
+       
 
 {posts.map(row => (
-    
+   
     <tr key={row.id} style={{ display: 'flex', justifyContent: 'center' }}>
         <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 300 }}>{row.grade}</td>
         <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 300 }}>{row.assingmentName}</td>
         <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 200 }}>{row.assingmentID}</td>
         <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 200 }}>{row.reviewerID}</td>
     </tr>
+   
 ))}
 
 {errorMsg ? <div>{errorMsg}</div> : null}
@@ -140,26 +135,35 @@ doSomething(){
                Top or Bottom:
 
                
-               <button
+               <button 
+            className={btn_classtype2}
+     
              title={"Top"}
            
              onClick={() => {
                this.setState({ Type: 'Top' });
-               this.setState({TopClicked:true });
+              // this.setState({TopClicked:false });
+              this.setState({TopsClicked: !this.state.TopsClicked})
+               console.log(this.state.TopsClicked +"Top")
              }}
               color="#841584"
-              
             >Top</button>
-         <button
+
+         <button 
+            className={btn_classtype}
+     
              title={"Bottom"}
            
              onClick={() => {
                this.setState({ Type: 'Bottom' });
-               this.setState({TopClicked:false });
-               console.log(this.state)
+              // this.setState({TopClicked:false });
+              this.setState({TopsClicked: !this.state.TopsClicked})
+               console.log(this.state.TopsClicked +"BOTTOM")
              }}
               color="#841584"
             >Bottom</button>
+
+      
     
       </div>
 
@@ -168,20 +172,24 @@ doSomething(){
 
                
                <button
+                className={btn_classIs2}
              title={"Projects"}
            
              onClick={() => {
                this.setState({ IsProject: 'true' });
-               this.setState({ProjectsClicked:true });
+              // this.setState({ProjectsClicked:true });
+               this.setState({ProjectsClicked: !this.state.ProjectsClicked})
              }}
               color="#841584"
             >Projects</button>
          <button
+           className={btn_classIs}
              title={"Reviewers"}
            
              onClick={() => {
                this.setState({IsProject:'false' });
-               this.setState({ProjectsClicked:false });
+             //  this.setState({ProjectsClicked:false });
+               this.setState({ProjectsClicked: !this.state.ProjectsClicked})
                console.log(this.state)
              }}
               color="#841584"
@@ -193,12 +201,12 @@ doSomething(){
                 <div>
                     <input type='text' name='N' value={N} onChange={this.changeHandler} placeholder='N'/>
                 </div>
-                <div>
+                {/* <div>
                     <input type='text' name='Type' value={Type}  onChange={this.changeHandler} placeholder='Enter:Top or Bottom'/>
                 </div>
                 <div>
                     <input type='text' name='IsProject' value={IsProject}  onChange={this.changeHandler} placeholder='Groupby: Projects or Reviewers'/>
-                </div>
+                </div> */}
                 <button type='submit'>Submit </button>
             </form>
             <div>{message}</div>

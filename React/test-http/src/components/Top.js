2f775660ae19b3a1b8d0c45b91ca2d0b7a295408
dimitrc <dimitrc@stud.ntnu.no>
2021-11-14 18:33:05 +0100

    errors not working

--- React/test-http/src/components/Top.js
+++ React/test-http/src/components/Top.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import TopInsides from './TopInsides'
 
 class Top extends Component {
     constructor(props) {
@@ -10,11 +11,12 @@ class Top extends Component {
             errorMsg: '',
             N:'',
             Type:'Top',
-            IsProject:false,
+            IsProject:true,
             site:'https://localhost:44344/api/Top/top',
             Submited:false,
             TopsClicked:false,
            ProjectsClicked:false,
+           Backup:false
           
          
         }
@@ -33,7 +35,7 @@ class Top extends Component {
 
 doSomething(){
       
-            console.log(this.state)
+          //  console.log(this.state)
            
            if(this.state.Submited===true){
             axios
@@ -41,7 +43,11 @@ doSomething(){
                 .then(response => {
                     console.log(response)
                     this.setState({ posts: response.data })
-                    this.setState({site:'https://localhost:44344/api/Top/top'})
+                  //  this.setState({site:'https://localhost:44344/api/Top/top'})
+                  this.setState({ errorMsg: '' })
+                  this.setState({ errorMsg2: '' })
+                  
+                 // TopInsides(this.state.posts,this.state.Backup)
                 })
                 .catch(error => {
                     console.log(error)
@@ -50,15 +56,15 @@ doSomething(){
                // this.setState({Submited:false})
            }
         }
-           
+         
         submitHandler = (e) => {
             this.setState({[e.target.name]:e.target.value})
-            
+            this.setState({IsProject:this.state.IsProject})
    
             if(this.state.N<1||this.state.N>300){
                 this.setState({ errorMsg2: ' Error in imput field 1. Provide possitive number (within reason)' })
             }
-         
+            this.setState({Backup:this.state.IsProject})
             this.setState({Submited:true},()=>{ this.doSomething()})
            // this.setState({site:this.state.site+'/'+this.state.N+'/'+this.state.Type+'/'+this.state.IsProject},()=>{ this.doSomething()})
           
@@ -72,7 +78,7 @@ doSomething(){
    
         onChangeValue(event){
             this.setState({Type:EventTarget.value})
-            console.log(this.state)
+            //console.log(this.state)
         }
         changeColorIs(){
             this.setState({ProjectsClicked: !this.state.ProjectsClicked})
@@ -80,7 +86,7 @@ doSomething(){
 
         changeColorType(){
             this.setState({TopsClicked: !this.state.TopsClicked})
-            console.log(this.state+"COLOR CHANGE!!")
+           // console.log(this.state+"COLOR CHANGE!!")
         }
 
     render() {
@@ -88,42 +94,53 @@ doSomething(){
         let btn_classIs = this.state.ProjectsClicked ? "blackButton" : "whiteButton";
         let btn_classIs2 = this.state.ProjectsClicked ? "whiteButton" : "blackButton" ;
         let btn_classtype2 = this.state.TopsClicked ? "whiteButton" : "blackButton" ;
-        const { posts, errorMsg,errorMsg2,N,Type,IsProject } = this.state
-        let n = 0
+        const { posts, errorMsg,errorMsg2,N, } = this.state
+      
+
+ 
+      
+    
+           
+
+
+
+
+
 
         let message
         if(this.state.Submited===false){
             message=<div>Please enter N,Top or Bottom and Projects or Reviewers</div>
         } else {
-            console.log(this.state)
-        message=  <div style={{ textAlign: 'center' }}>
+            console.log(this.state+"HERE!!")
+        message= 
+         <div style={{ textAlign: 'center' }}>
           
-    <tr style={{ display: 'flex', justifyContent: 'center' }}>
-      <td style={{ border: "1px solid rgb(0, 0, 0)", width: 300 }}>Grade</td>
-     <td style={{ border: "1px solid rgb(0, 0, 0)", width: 300 }}>Assignment name</td>
-     <td style={{ border: "1px solid rgb(0, 0, 0)", width: 200 }}>Assignment ID</td>
-     <td style={{ border: "1px solid rgb(0, 0, 0)", width: 200 }}>Reviewer ID</td>
+         {/* <tr style={{ display: 'flex', justifyContent: 'center' }}>
+              <td style={{ border: "1px solid rgb(0, 0, 0)", width: 300 }}>Grade</td>
+             <td style={{ border: "1px solid rgb(0, 0, 0)", width: 300 }}>Assignment name</td>
+             <td style={{ border: "1px solid rgb(0, 0, 0)", width: 200 }}>Assignment ID</td>
+             <td style={{ border: "1px solid rgb(0, 0, 0)", width: 200 }}>Reviewer ID</td>
+         </tr> */}
+       
+              {TopInsides(posts,this.state.Backup)}
+    {/* {posts.map(row => (
+        
+         <tr key={row.id} style={{ display: 'flex', justifyContent: 'center' }}>
+               <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 300 }}>{row.grade}</td>          
+              <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 300 }}>{row.assingmentName}</td>
+              <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 200 }}>{row.assingmentID}</td>
+              <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n++ % 2) === 1 ? '#aae' : '#dde', width: 200 }}>{row.reviewerID}</td>
+          </tr>
+   
+            ))} */}
 
-        </tr>
        
 
-{posts.map(row => (
    
-    <tr key={row.id} style={{ display: 'flex', justifyContent: 'center' }}>
-        <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 300 }}>{row.grade}</td>
-        <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 300 }}>{row.assingmentName}</td>
-        <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 200 }}>{row.assingmentID}</td>
-        <td style={{ border: "1px solid rgb(0, 0, 0)", backgroundColor: (n % 2) === 1 ? '#aae' : '#dde', width: 200 }}>{row.reviewerID}</td>
-    </tr>
-   
-))}
-
-{errorMsg ? <div>{errorMsg}</div> : null}
-{errorMsg2 ? <div>{errorMsg2}</div> : null}
 
 
-</div>
-        }
+    </div>
+    }
 
 
         return (
@@ -143,8 +160,8 @@ doSomething(){
              onClick={() => {
                this.setState({ Type: 'Top' });
               // this.setState({TopClicked:false });
-              this.setState({TopsClicked: !this.state.TopsClicked})
-               console.log(this.state.TopsClicked +"Top")
+             if(this.state.TopsClicked===true){ this.setState({TopsClicked: !this.state.TopsClicked})}
+               console.log(this.state.Type +"Top")
              }}
               color="#841584"
             >Top</button>
@@ -157,8 +174,8 @@ doSomething(){
              onClick={() => {
                this.setState({ Type: 'Bottom' });
               // this.setState({TopClicked:false });
-              this.setState({TopsClicked: !this.state.TopsClicked})
-               console.log(this.state.TopsClicked +"BOTTOM")
+              if(this.state.TopsClicked===false){ this.setState({TopsClicked: !this.state.TopsClicked})}
+               console.log(this.state.Type +"BOTTOM")
              }}
               color="#841584"
             >Bottom</button>
@@ -178,7 +195,8 @@ doSomething(){
              onClick={() => {
                this.setState({ IsProject: 'true' });
               // this.setState({ProjectsClicked:true });
-               this.setState({ProjectsClicked: !this.state.ProjectsClicked})
+              if(this.state.ProjectsClicked===true){ this.setState({ProjectsClicked: !this.state.ProjectsClicked})}
+               
              }}
               color="#841584"
             >Projects</button>
@@ -189,7 +207,7 @@ doSomething(){
              onClick={() => {
                this.setState({IsProject:'false' });
              //  this.setState({ProjectsClicked:false });
-               this.setState({ProjectsClicked: !this.state.ProjectsClicked})
+             if(this.state.ProjectsClicked===false){ this.setState({ProjectsClicked: !this.state.ProjectsClicked})}
                console.log(this.state)
              }}
               color="#841584"
@@ -210,8 +228,9 @@ doSomething(){
                 <button type='submit'>Submit </button>
             </form>
             <div>{message}</div>
-        <div>Here???</div>
-           
+      
+            {errorMsg ? <div style={{border: 'solid', borderColor:'red'}} >{errorMsg}</div> : null}
+        {errorMsg2 ? <div style={{border: 'solid', borderColor:'red'}} >{errorMsg2}</div> : null}
           
         </div>
         )

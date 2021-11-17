38b180c79c5c1f9b079cd27df8ab99908b560781
ScarredSceptile <jonas.haegd@gmail.com>
2021-11-17 11:52:42 +0100

    Format and shorten TopController.cs

--- Backend/CSAMS/Controllers/TopController.cs
+++ Backend/CSAMS/Controllers/TopController.cs
@@ -1,21 +1,22 @@
-﻿using CSAMS.Models;
+﻿using CSAMS.APIModels;
+using CSAMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
-using CSAMS.APIModels;
-using System;
 
 namespace CSAMS.Controllers
 {
-
+    /// <summary>
+    /// Top/Bottom N Projects based on mean average or Reviewers based on distance from mean average
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class TopController : ControllerBase
     {
         private readonly AppDbContext _context;
 
-        public int N { get; set; }
         public TopController(AppDbContext context)
         {
             _context = context;
@@ -26,234 +27,186 @@ namespace CSAMS.Controllers
             return N;
         }
 
-
-
-
-
-
-     
-       // [HttpGet("Top/{N}/{Type}/{IsProject}")]
-        public static TopModel TopProjects(UserReviews[] userReviews, Fields[] fields, Boolean IsProject,float average)
+        /// <summary>
+        /// Get the top/bottom Reviewers or Projects
+        /// </summary>
+        /// <param name="N">Amount of projects/reviewers wanted</param>
+        /// <param name="Type">Top or Bottom</param>
+        /// <param name="IsProject">Is it project or reviewers</param>
+        /// <returns>Array of TopModel</returns>
+        [HttpGet("Top/{N}/{Type}/{IsProject}")]
+        public async Task<ActionResult<TopModel[]>> GetTopReviews(int N, string Type, bool IsProject)
         {
+            var fields = await _context.Fields.ToArrayAsync();
+            var userdata = _context.UserReviews
+                .Include(r => r.Review)
+                .Include(r => r.Assignment)
+                .AsEnumerable()
+                .ToArray();
 
+            var toAverege = _context.UserReviews.Include(r => r.Review).ToArray();
+            var ret = OuterTopProjects(N, Type, IsProject, userdata, fields);
 
-            var child =
-                userReviews
+            if (ret is null)
+            {
+                return BadRequest("Bad request");
+            }
+
+            if (ret.Length == 0)
+            {
+                return BadRequest("0 length");
+            }
+
+            return ret;
+        }
+
+        // [HttpGet("Top/{N}/{Type}/{IsProject}")]
+        public static TopModel TopProjects(UserReviews[] userReviews, Fields[] fields, bool IsProject, float average)
+        {
+            var child = userReviews
                 .Select(r => (r, fields.Where(f => f.FormID == r.Review.FormID)
                 .Where(f => f.Name == r.Name).FirstOrDefault().Weight))
                 .Where(ur => ur.r.Answer != null)
                 .Where(ur => ur.r.Assignment != null)
                 .Where(ur => ur.r.Type == "radio")
-                .Where(ur=>ur.Item2!=0)
+                .Where(ur => ur.Item2 != 0)
                 .ToArray();
-                  
-            if (IsProject == true)
+
+            TopModel[] grades;
+
+            if (IsProject)
             {
-               var child2= child
-                     .Select(ur => new TopModel
-                     {
-                         Grade = float.Parse(ur.r.Answer) * ur.Item2,
-                         AssingmentID = ur.r.AssignmentID,
-                         AssingmentName = ur.r.Assignment.Name,
-                         ReviewerID = ur.r.UserReviewer,
-                         type = "Top"
-
-                     })
-                    .ToArray();
-
-
-                Console.WriteLine(child.Length);
-             
-                
-                if (child.Length == 0)
-                    return null;
-                Console.WriteLine(child2[0].ReviewerID);
-                var ret = child2.Sum(r => r.Grade) / child.Sum(ur => ur.Item2);
-                TopModel model = new TopModel { Grade = MathF.Round(ret, 2), AssingmentID = child2[0].AssingmentID, AssingmentName = child2[0].AssingmentName,ReviewerID=child2[0].ReviewerID, type = "top" };
-                return model;
+                grades = child
+                      .Select(ur => new TopModel
+                      {
+                          Grade = float.Parse(ur.r.Answer) * ur.Item2,
+                          AssingmentID = ur.r.AssignmentID,
+                          AssingmentName = ur.r.Assignment.Name,
+                          ReviewerID = ur.r.UserReviewer,
+                          type = "Top"
+                      })
+                     .ToArray();
             }
             else
             {
-                {
-                    var child2 = child
-                          .Select(ur => new TopModel
-                          {
-                              Grade = MathF.Abs(float.Parse(ur.r.Answer) * ur.Item2 - average),
-                              AssingmentID = ur.r.AssignmentID,
-                              AssingmentName = ur.r.Assignment.Name,
-                              ReviewerID = ur.r.UserReviewer,
-                              type = "Top"
-
-                          })
-                         .ToArray();
-
-
-                    Console.WriteLine(child.Length);
-                    if (child.Length == 0)
-                        return null;
-                    var ret = child2.Sum(r => r.Grade)/child.Sum(ur => ur.Item2);
-                    TopModel model = new TopModel { Grade = MathF.Round(ret, 2), AssingmentID = child2[0].AssingmentID, AssingmentName = child2[0].AssingmentName, ReviewerID = child2[0].ReviewerID, type = "top" };
-                    return model;
-                }
+                grades = child
+                      .Select(ur => new TopModel
+                      {
+                          Grade = MathF.Abs(float.Parse(ur.r.Answer) * ur.Item2 - average),
+                          AssingmentID = ur.r.AssignmentID,
+                          AssingmentName = ur.r.Assignment.Name,
+                          ReviewerID = ur.r.UserReviewer,
+                          type = "Top"
+                      })
+                     .ToArray();
             }
 
+            if (child.Length == 0)
+            {
+                return null;
+            }
 
-        
+            var ret = grades.Sum(r => r.Grade) / child.Sum(ur => ur.Item2);
+
+            TopModel model = new TopModel
+            {
+                Grade = MathF.Round(ret, 2),
+                AssingmentID = grades[0].AssingmentID,
+                AssingmentName = grades[0].AssingmentName,
+                ReviewerID = grades[0].ReviewerID,
+                type = "top"
+            };
+
+            return model;
         }
 
-        // await _context.UserReviews.Include(i => i.Assignment).Include(i => i.Reviewer).Include(i => i.Target).Include(i => i.Review).Where(ur => ur.Comment != null) .Take(5).ToArrayAsync();
-
-
-
-
-
+        /// <summary>
+        /// Get the mean average
+        /// </summary>
+        /// <param name="userReviews">Reviews with the answers</param>
+        /// <param name="fields">Fields to know the weight of answers</param>
+        /// <returns>Mean Average</returns>
         public static float GetMiddle(UserReviews[] userReviews, Fields[] fields)
         {
-            var child =
-                    userReviews
-                    .Select(r => (r, fields
-                    .Where(f => f.FormID == r.Review.FormID)
-                    .Where(f => f.Name == r.Name)
-                    .FirstOrDefault().Weight))
+            var child = userReviews
+                    .Select(r => (r, fields.Where(
+                         f => f.FormID == r.Review.FormID)
+                        .Where(f => f.Name == r.Name)
+                        .FirstOrDefault().Weight))
                     .Where(ur => ur.r.Answer != null)
                     .Where(ur => ur.r.Type == "radio").Where(ur => ur.Item2 != 0);
+
             var child2 = child
-                    .OrderBy(ur => ur.r.UserTarget).OrderByDescending(ur => int.Parse(ur.r.Answer) * ur.Item2)
-                   .Select(ur => new TopModel
-                   {
-                       Grade = float.Parse(ur.r.Answer) * ur.Item2
-                   })
+                    .Select(ur => new TopModel
+                    {
+                        Grade = float.Parse(ur.r.Answer) * ur.Item2
+                    })
                     .ToArray();
 
             if (child2.Length == 0)
+            {
                 return 0;
-            return child2.Sum(r => r.Grade)/child.Sum(ur => ur.Item2);
+            }
+
+            return child2.Sum(r => r.Grade) / child.Sum(ur => ur.Item2);
         }
 
-
-
-        [HttpGet("Top/{N}/{Type}/{IsProject}")]
-        public async Task<ActionResult<TopModel[]>> GetTopReviews(int N, string Type, Boolean IsProject)
+        /// <summary>
+        /// Get the top/bottom Reviewers or Projects
+        /// </summary>
+        /// <param name="N"></param>
+        /// <param name="Type"></param>
+        /// <param name="IsProject"></param>
+        /// <param name="Userdata"></param>
+        /// <param name="fields"></param>
+        /// <returns>Array of TopModel</returns>
+        public static TopModel[] OuterTopProjects(int N, string Type, bool IsProject, UserReviews[] Userdata, Fields[] fields)
         {
-            var fields = await _context.Fields.ToArrayAsync();
-            var userdata = _context.UserReviews.Include(r => r.Review).Include(r => r.Assignment)
-                  .AsEnumerable()
-                  // .GroupBy(r => r.AssignmentID)
-                  // .Select(r => TopProjects(r.ToArray(), fields, IsProject))
-                  .Where(p => p != null)
-
-                  .ToArray();
-            var toAverege = _context.UserReviews.Include(r => r.Review).ToArray();
-            
-            var ret = OuterTopProjects(N, Type, IsProject, userdata, fields, toAverege);
-
-            if (ret.Length == 0)
-                return BadRequest("0 length");
-
-            return ret;
-        }
-
-
-        public static TopModel[] OuterTopProjects(int N, string Type, Boolean IsProject, CSAMS.Models.UserReviews[] Userdata, CSAMS.Models.Fields[] fields,UserReviews[] ToAvrege)
-        {
-            var t = Userdata
-
-                  ;
-            Console.WriteLine($"N is {N} and Type is {Type}");
-            Console.WriteLine(Userdata.Length + " testdata_length ");
-           
-            if (IsProject == true)
+            if (IsProject)
             {
-                if (Type == "Top")
-                {
-                    return t
-                           .GroupBy(r => r.AssignmentID)
-                          .Select(r => TopController.TopProjects(r.ToArray(), fields, IsProject, 0))
-                       .Where(r => r != null)
-
-                         .OrderByDescending(p => p.Grade)
-                         .Take(N)
-                         .ToArray();
-                    ;
-                }
-                else if (Type == "Bottom")
-                {
-                    Console.WriteLine(t.Length);
-                    var x = t
+                var ret = Userdata
                         .GroupBy(r => r.AssignmentID)
-                       .Select(r => TopController.TopProjects(r.ToArray(), fields, IsProject, 0))
-                       .Where(r => r != null)
-                       .OrderBy(p => p.Grade)
-                       .Take(N)
-
-                       .ToArray();
-                    return x;
-                }
-                else
-                {
-                    return t
-                          .GroupBy(r => r.AssignmentID)
-                          .Select(r => TopController.TopProjects(r.ToArray(), fields, IsProject, 0))
-                       .Where(r => r != null)
-                         .Take(2)
-                         .ToArray();
-                    ;
-                }
-            }
-            else////////////////////////////
-            {
-                var average = TopController.GetMiddle(ToAvrege, fields);
+                        .Select(r => TopProjects(r.ToArray(), fields, IsProject, 0))
+                        .Where(r => r != null);
 
                 if (Type == "Top")
                 {
-                    return t
-
-                           .GroupBy(r => r.UserReviewer)
-                          .Select(r => TopController.TopProjects(r.ToArray(), fields, IsProject, average))
-
-                       .Where(r => r != null)
-
-                         .OrderBy(p => p.Grade)
-
-                         .Take(N)
-
-                         .ToArray();
-                    ;
+                    ret = ret.OrderByDescending(p => p.Grade);
                 }
                 else if (Type == "Bottom")
                 {
-                    return t
-                        .GroupBy(r => r.UserReviewer)
-                       .Select(r => TopController.TopProjects(r.ToArray(), fields, IsProject, average))
-                       .Where(r => r != null)
-                       .OrderByDescending(p => p.Grade)
-                       .Take(N)
-                       .ToArray();
-
+                    ret = ret.OrderBy(p => p.Grade);
                 }
                 else
                 {
-                    return t
-                          .GroupBy(r => r.ReviewID)
-                          .Select(r => TopController.TopProjects(r.ToArray(), fields, IsProject, average))
-                       .Where(r => r != null)
-                         .Take(2)
-                         .ToArray();
-                    ;
+                    return null;
                 }
+                return ret.Take(N).ToArray();
             }
+            else
+            {
+                var average = GetMiddle(Userdata, fields);
 
+                var ret = Userdata
+                        .GroupBy(r => r.UserReviewer)
+                        .Select(r => TopProjects(r.ToArray(), fields, IsProject, average))
+                        .Where(r => r != null);
 
+                if (Type == "Top")
+                {
+                    ret = ret.OrderBy(p => p.Grade);
+                }
+                else if (Type == "Bottom")
+                {
+                    ret = ret.OrderByDescending(p => p.Grade);
+                }
+                else
+                {
+                    return null;
+                }
+
+                return ret.Take(N).ToArray();
+            }
         }
-
-
-   
-
     }
-
-
-
 }
-
-
-
